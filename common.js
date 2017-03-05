bae792239cf3103d47b405a0ba8b3e7e5975dc90
Kirigam <hrytsiv.a.s@gmail.com>
2017-03-05 16:09:13 +0200

    Add class controller

--- common.js
+++ common.js
@@ -64,3 +64,62 @@ var model = {
     return true;
   }
 };
+
+var controller = {
+  guesses: 0,
+
+  processGuess: function (guess) {
+    var location = this.parseGuess(guess);
+
+    if(location)
+    {
+      this.guesses++;
+      var hit = model.fire(location);
+
+      if(hit && model.shipsSunk === model.numShip)
+      {
+        view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses.")
+      }
+    } // end if(location)
+  },
+
+  parseGuess: function (guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+    if(guess === null ||guess.length !== 2) {
+      alert("Oops, please enter a letter and a number on the board.");
+    }
+    else {
+      var firstChar = guess.charAt(0);
+      firstChar.toUpperCase();
+      var row = alphabet.indexOf(firstChar);
+      var column = guess.charAt(1);
+
+      if(isNaN(row) || isNaN(column)) {
+        alert("Oops, that isn't on the board.")
+      }
+      else if (row < 0 || row >= model.boardSize) {
+        alert("Oops, that's off the board!");
+      }
+      else if (column < 0 || column >= model.boardSize) {
+        alert("Oops, that's off the board!");
+      }
+      else {
+        return row + column;
+      }
+    }
+    return null;
+  }
+};
+
+
+
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
